e92238885682136be2ac789a81acbe6df2a6fc60
BenjaminAlvarezDiaz <benja77744@gmail.com>
2022-10-01 18:43:18 -0300

    Cambiando a animated container

--- lib/component/list_wheel_horizontal.dart
+++ lib/component/list_wheel_horizontal.dart
@@ -22,22 +22,166 @@ class _ListWheelState extends State<ListWheelHorizontal> {
           itemBuilder: (_, i) {
             return Transform.scale(
               scale: i == _index ? 1 : 0.7,
-              child: Card(
-                elevation: 6,
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                child: Center(
-                  child: Text(
-                    "Card ${i + 1}",
-                    style: TextStyle(fontSize: 32),
-                  ),
+              child: AnimatedContainer(
+                duration: Duration(seconds: 1),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
                 ),
-              ),
+                child: Text(
+                  'Card ${i+1}',
+                  style: TextStyle(fontSize: 32),
+                )
+              )
             );
           },
         ),
       ),
     );
   }
+
+  /*Card card(){
+    return Card(
+      elevation: 6,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      child: Center(
+        child: Text(
+          "Card ${i + 1}",
+          style: const TextStyle(fontSize: 32),
+        ),
+      ),
+    );
+  }*/
+
+Container leo(){
+  return Container(
+    height: 400,
+    width: double.infinity,
+    color: Colors.white,
+
+    child: PageView(
+      scrollDirection: Axis.horizontal,
+      controller: PageController(
+        viewportFraction: 0.5,
+      ),
+      pageSnapping: true,
+
+      children: [
+        Container(
+          margin: const EdgeInsets.all(20) ,
+          height: 100,
+          width: 200,
+          child: const Material(
+            elevation: 20,
+            color: Colors.teal,
+          ),
+        ),
+
+        Container(
+          margin: const EdgeInsets.all(20) ,
+          height: 100,
+          width: 200,
+          child: const Material(
+            elevation: 20,
+            color: Colors.purpleAccent,
+          ),
+        ),
+
+        Container(
+          margin: const EdgeInsets.all(20) ,
+          height: 100,
+          width: 200,
+          child: const Material(
+            elevation: 20,
+            color: Colors.black,
+          ),
+        ),
+
+        Container(
+          margin: const EdgeInsets.all(20) ,
+          height: 100,
+          width: 200,
+          child: const Material(
+            elevation: 20,
+            color: Colors.blue,
+          ),
+        ),
+
+        Container(
+          margin: const EdgeInsets.all(20) ,
+          height: 100,
+          width: 200,
+          child: const Material(
+            elevation: 20,
+            color: Colors.black,
+          ),
+        ),
+
+        Container(
+          margin: const EdgeInsets.all(20) ,
+          height: 100,
+          width: 200,
+          child: const Material(
+            elevation: 20,
+            color: Colors.yellowAccent,
+          ),
+        ),
+
+        Container(
+          margin: const EdgeInsets.all(20) ,
+          height: 100,
+          width: 200,
+          child: const Material(
+            elevation: 20,
+            color: Colors.black,
+          ),
+        ),
+
+        Container(
+          margin: const EdgeInsets.all(20) ,
+          height: 100,
+          width: 200,
+          child: const Material(
+            elevation: 20,
+            color: Colors.yellowAccent,
+          ),
+        ),
+
+        Container(
+          margin: const EdgeInsets.all(20) ,
+          height: 100,
+          width: 200,
+          child: const Material(
+            elevation: 20,
+            color: Colors.black,
+          ),
+        ),
+
+        Container(
+          margin: const EdgeInsets.all(20) ,
+          height: 100,
+          width: 200,
+          child: const Material(
+            elevation: 20,
+            color: Colors.yellowAccent,
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.all(20) ,
+          height: 100,
+          width: 200,
+          child: const Material(
+            elevation: 20,
+            color: Colors.black,
+          ),
+        ),
+      ],
+    ),
+  );}
+
+
+
+
+
 }
 
 
