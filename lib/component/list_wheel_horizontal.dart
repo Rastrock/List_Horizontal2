a604b6cd53f0b6e8339255f3bdfa596c0e9200f7
Leonel Coronel <leeoneel987@gmail.com>
2022-10-03 20:16:01 -0300

    Terminado supongo

--- lib/component/list_wheel_horizontal.dart
+++ lib/component/list_wheel_horizontal.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
+import 'package:flutter/material.dart';
 class ListWheelHorizontal extends StatefulWidget {
   const ListWheelHorizontal({Key? key}) : super(key: key);
 
@@ -9,137 +10,122 @@ class ListWheelHorizontal extends StatefulWidget {
 
 class _ListWheelState extends State<ListWheelHorizontal> {
 
-  PageController controller = PageController(initialPage: 0,keepPage: true);
+  final _controller = PageController(viewportFraction: 0.5);
+  double _currentPage = 0.0;
+
+  void _listener(){
+    setState(() {
+      _currentPage = _controller.page!;
+    });
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    _controller.addListener(_listener);
+  }
+
+  @override
+  void dispose(){
+    _controller.removeListener(_listener);
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 400,
-      width: double.infinity,
-      color: Colors.white,
 
-      child: PageView(
-        scrollDirection: Axis.horizontal,
-        controller: PageController(
-          viewportFraction: 0.5,
+    double _opty;
+
+    List<String> text =[
+      "Name cat 1",
+      "Name cat 2",
+      "Name cat 3",
+      "Name cat 4",
+      "Name cat 5",
+      "Name cat? 6",
+    ];
+
+    List<String> image = [
+      "images/cat1.jpg",
+      "images/cat2.jpg",
+      "images/cat3.jpg",
+      "images/cat4.jpg",
+      "images/cat5.jpg",
+      "images/pajaro.jpg"
+    ];
+
+    return Center(
+      child: SizedBox(
+        height: 500, // card height
+        child: PageView.builder(
+          itemCount: image.length,
+          controller: _controller,
+          itemBuilder: (_,i) {
+            if(i == _currentPage){
+              _opty =1;
+              return Transform.scale(
+                scale: 1.1,
+                child: _pageimages(image[i],text[i], _opty),
+              );
+            }
+            else if(i < _currentPage){
+              _opty = max(1 - (_currentPage - i), 0.5);
+              return Transform.scale(
+                scale: max(1.1 - (_currentPage - i), 0.8),
+                child: _pageimages(image[i], text[i], _opty),
+              );
+            }
+            else {
+              _opty = max(1 - ( i - _currentPage), 0.5);
+              return Transform.scale(
+                scale: max(1.1 - (i - _currentPage), 0.8),
+                child: _pageimages(image[i],text[i], _opty),
+              );
+            }
+          }
         ),
-        pageSnapping: true,
-
-        children: [
-          Container(
-            margin: const EdgeInsets.all(20) ,
-            height: 100,
-            width: 200,
-            child: const Material(
-              elevation: 20,
-              color: Colors.teal,
-            ),
-          ),
-
-          Container(
-            margin: const EdgeInsets.all(20) ,
-            height: 100,
-            width: 200,
-            child: const Material(
-              elevation: 20,
-              color: Colors.purpleAccent,
-            ),
-          ),
-
-          Container(
-            margin: const EdgeInsets.all(20) ,
-            height: 100,
-            width: 200,
-            child: const Material(
-              elevation: 20,
-              color: Colors.black,
-            ),
-          ),
-
-          Container(
-            margin: const EdgeInsets.all(20) ,
-            height: 100,
-            width: 200,
-            child: const Material(
-              elevation: 20,
-              color: Colors.blue,
-            ),
-          ),
-
-          Container(
-            margin: const EdgeInsets.all(20) ,
-            height: 100,
-            width: 200,
-            child: const Material(
-              elevation: 20,
-              color: Colors.black,
-            ),
-          ),
-
-          Container(
-            margin: const EdgeInsets.all(20) ,
-            height: 100,
-            width: 200,
-            child: const Material(
-              elevation: 20,
-              color: Colors.yellowAccent,
-            ),
-          ),
-
-          Container(
-            margin: const EdgeInsets.all(20) ,
-            height: 100,
-            width: 200,
-            child: const Material(
-              elevation: 20,
-              color: Colors.black,
-            ),
-          ),
-
-          Container(
-            margin: const EdgeInsets.all(20) ,
-            height: 100,
-            width: 200,
-            child: const Material(
-              elevation: 20,
-              color: Colors.yellowAccent,
-            ),
-          ),
-
-          Container(
-            margin: const EdgeInsets.all(20) ,
-            height: 100,
-            width: 200,
-            child: const Material(
-              elevation: 20,
-              color: Colors.black,
-            ),
-          ),
-
-          Container(
-            margin: const EdgeInsets.all(20) ,
-            height: 100,
-            width: 200,
-            child: const Material(
-              elevation: 20,
-              color: Colors.yellowAccent,
-            ),
-          ),
-
-          Container(
-            margin: const EdgeInsets.all(20) ,
-            height: 100,
-            width: 200,
-            child: const Material(
-              elevation: 20,
-              color: Colors.black,
-            ),
-          ),
-
-        ],
       ),
     );
   }
+}
+
+_pageimages(String image, String text, double opty){
+  return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 10.0) ,
+        child:  Material(
+          elevation: 10,
+          shadowColor: Colors.black,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Column(
+              children: [
+                     ClipRRect(
+                      child: Container(
+                        height: 180,
+                        width: 300,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(25.0),
+                            image: DecorationImage(
+                              image: AssetImage(image),
+                              fit: BoxFit.cover,
+                              opacity: opty,
+                            )
+                        ),
+                      ),
+                    ),
+
+                const SizedBox(height: 20),
+                Text(text),
+
+              ],
+            ),
+          ),
+    )
+  );
 }
 
 
