077401cce1dea3a0bb73f02e2fc4a364a89d3e6b
BenjaminAlvarezDiaz <benja77744@gmail.com>
2022-10-03 21:36:08 -0300

    Haciéndolo mas escalable

--- lib/component/list_wheel_horizontal.dart
+++ lib/component/list_wheel_horizontal.dart
@@ -2,31 +2,61 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 class ListWheelHorizontal extends StatefulWidget {
-  const ListWheelHorizontal({Key? key}) : super(key: key);
+  late List<String> directoryImage;
+  late List<String> textCard;
+  late List<Widget> buttonCard;
+  late double? heightCard;
+  ListWheelHorizontal({
+    Key? key,
+    required double? height,
+    required List<String> directory,
+    required List<String> text,
+    required List<Widget> button
+  }) : super(key: key){
+
+    directoryImage = directory;
+    textCard = text;
+    buttonCard = button;
+    heightCard = height;
+}
 
   @override
-  State<ListWheelHorizontal> createState() => _ListWheelState();
+  State<ListWheelHorizontal> createState() => _ListWheelState(heightCard, directoryImage, textCard, buttonCard);
 }
 
 class _ListWheelState extends State<ListWheelHorizontal> {
+  late double? heightCard;
+  late List<String> directoryCard;
+  late List<String> textCard;
+  late List<Widget> buttonCard;
+  _ListWheelState(
+      double? height,
+      List<String> directory,
+      List<String> text,
+      List<Widget> button){
+    heightCard = height;
+    directoryCard = directory;
+    textCard = text;
+    buttonCard = button;
 
+  }
   final _controller = PageController(viewportFraction: 0.5);
   double _currentPage = 0.0;
 
-  void _listener(){
+  void _listener() {
     setState(() {
       _currentPage = _controller.page!;
     });
   }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     _controller.addListener(_listener);
   }
 
   @override
-  void dispose(){
+  void dispose() {
     _controller.removeListener(_listener);
     _controller.dispose();
     super.dispose();
@@ -34,66 +64,46 @@ class _ListWheelState extends State<ListWheelHorizontal> {
 
   @override
   Widget build(BuildContext context) {
-
     double _opty;
-
-    List<String> text =[
-      "Name cat 1",
-      "Name cat 2",
-      "Name cat 3",
-      "Name cat 4",
-      "Name cat 5",
-      "Name cat? 6",
-    ];
-
-    List<String> image = [
-      "images/cat1.jpg",
-      "images/cat2.jpg",
-      "images/cat3.jpg",
-      "images/cat4.jpg",
-      "images/cat5.jpg",
-      "images/pajaro.jpg"
-    ];
-
     return Center(
       child: SizedBox(
-        height: 500, // card height
+        height: heightCard, // card height
         child: PageView.builder(
-          itemCount: image.length,
-          controller: _controller,
-          itemBuilder: (_,i) {
-            if(i == _currentPage){
-              _opty =1;
-              return Transform.scale(
-                scale: 1.1,
-                child: _pageimages(image[i],text[i], _opty),
-              );
+            itemCount: directoryCard.length,
+            controller: _controller,
+            itemBuilder: (_, i) {
+              if (i == _currentPage) {
+                _opty = 1;
+                return Transform.scale(
+                  scale: 1.1,
+                  child: _pageimages(directoryCard[i], textCard[i], buttonCard[i], _opty),
+                );
+              }
+              else if (i < _currentPage) {
+                _opty = max(1 - (_currentPage - i), 0.5);
+                return Transform.scale(
+                  scale: max(1.1 - (_currentPage - i), 0.8),
+                  child: _pageimages(directoryCard[i], textCard[i], buttonCard[i], _opty),
+                );
+              }
+              else {
+                _opty = max(1 - (i - _currentPage), 0.5);
+                return Transform.scale(
+                  scale: max(1.1 - (i - _currentPage), 0.8),
+                  child: _pageimages(directoryCard[i], textCard[i], buttonCard[i], _opty),
+                );
+              }
             }
-            else if(i < _currentPage){
-              _opty = max(1 - (_currentPage - i), 0.5);
-              return Transform.scale(
-                scale: max(1.1 - (_currentPage - i), 0.8),
-                child: _pageimages(image[i], text[i], _opty),
-              );
-            }
-            else {
-              _opty = max(1 - ( i - _currentPage), 0.5);
-              return Transform.scale(
-                scale: max(1.1 - (i - _currentPage), 0.8),
-                child: _pageimages(image[i],text[i], _opty),
-              );
-            }
-          }
         ),
       ),
     );
   }
-}
 
-_pageimages(String image, String text, double opty){
-  return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 10.0) ,
-        child:  Material(
+
+  _pageimages(String image, String text, Widget button,  double opty) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 10.0),
+        child: Material(
           elevation: 10,
           shadowColor: Colors.black,
           child: Container(
@@ -101,31 +111,33 @@ _pageimages(String image, String text, double opty){
               borderRadius: BorderRadius.circular(20),
             ),
             child: Column(
-              children: [
-                     ClipRRect(
-                      child: Container(
-                        height: 180,
-                        width: 300,
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(25.0),
-                            image: DecorationImage(
-                              image: AssetImage(image),
-                              fit: BoxFit.cover,
-                              opacity: opty,
-                            )
-                        ),
-                      ),
+              children: <Widget>[
+                ClipRRect(
+                  child: Container(
+                    height: 180,
+                    width: 300,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(25.0),
+                        image: DecorationImage(
+                          image: AssetImage(image),
+                          fit: BoxFit.cover,
+                          opacity: opty,
+                        )
                     ),
+                  ),
+                ),
 
                 const SizedBox(height: 20),
                 Text(text),
-
+                const SizedBox(height: 20),
+                button
               ],
             ),
           ),
-    )
-  );
+        )
+    );
+  }
 }
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,44 @@ import 'package:flutter/material.dart';
 import 'package:list_element/component/list_wheel_horizontal.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  List<String> image = [
+    "images/cat1.jpg",
+    "images/cat2.jpg",
+    "images/cat3.jpg",
+    "images/cat4.jpg",
+    "images/cat5.jpg",
+    "images/pajaro.jpg"
+  ];
+
+  List<String> text = [
+    "Name cat 1",
+    "Name cat 2",
+    "Name cat 3",
+    "Name cat 4",
+    "Name cat 5",
+    "Name cat? 6",
+  ];
+  List<Widget> button = [
+    ElevatedButton(onPressed: (){}, child: Container(color: Colors.purple)),
+    ElevatedButton(onPressed: (){}, child: Container(color: Colors.blue)),
+    ElevatedButton(onPressed: (){}, child: Container(color: Colors.red)),
+    ElevatedButton(onPressed: (){}, child: Container(color: Colors.black)),
+    ElevatedButton(onPressed: (){}, child: Container(color: Colors.orange)),
+    ElevatedButton(onPressed: (){}, child: Container(color: Colors.greenAccent)),
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
         appBar: AppBar(),
-        body: const ListWheelHorizontal(),
+        body: ListWheelHorizontal(height: 350, directory: image, text: text, button: button),
       ),
     );
   }
